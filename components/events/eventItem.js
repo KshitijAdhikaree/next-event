d8d8aae8600209de4f4c97fc9982b801ba4a8a33
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-24 18:37:46 +0545

    added button

--- components/events/eventItem.js
+++ components/events/eventItem.js
@@ -1,18 +1,21 @@
-import Link from 'next/link'
 import styles from './eventItem.module.css'
+import Button from '../ui/button'
+import DateIcon from '../icons/dateIcon'
+import AddressIcon from '../icons/addressIcon'
+import ArrowRightIcon from '../icons/arrowRightIcon'
 
 function EventItem(props) {
-    const {title, image, date, location, id} = props
+  const { title, image, date, location, id } = props
 
-    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    })
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  })
 
-    const formattedAddress = location.replace(',','\n')
+  const formattedAddress = location.replace(',', '\n')
 
-    const exploreLink = `/events/${id}`
+  const exploreLink = `/events/${id}`
 
   return (
     <li className={styles.item}>
@@ -22,14 +25,23 @@ function EventItem(props) {
           <h2>{title}</h2>
 
           <div className={styles.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
+
           <div className={styles.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
+
         <div className={styles.actions}>
-          <Link href={exploreLink}>Explore Events</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={styles.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
